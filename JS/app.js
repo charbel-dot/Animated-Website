6ca2c47b0c585a8929958a87a930abca26c24210
charbel-dot <nassehcharbel@gmail.com>
2021-05-22 14:00:31 +0300

    Created a random number generator and styled it

--- JS/app.js
+++ JS/app.js
@@ -4,3 +4,15 @@ button.addEventListener("click", toggle);
 function toggle(){
     button.classList.toggle('samir');
 };
+
+
+
+const random_button = document.getElementById('random-button');
+
+random_button.addEventListener("click", generate);
+
+function generate(){
+    let number = Math.floor(Math.random () * 10000);
+    let random = document.getElementById('random-number').innerHTML = number;
+};
+generate();
